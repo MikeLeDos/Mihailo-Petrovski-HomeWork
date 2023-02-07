a3008db5f8d6a56ba68aef25cbef7f38f415a2cc
MikeLeDos <mikipetrovski@gmail.com>
2023-02-07 19:18:18 +0100

    Homework8

---
+++ Homework8/homework8.js
@@ -0,0 +1,14 @@
+let nameAndSurname= "Lionel Messi";
+let age= 35;
+let jobTitle= "programmer";
+let gender="male";
+let youLiveInSkopje="true";
+
+
+let person = {
+    nameAndSurname:"Lionel Messi",
+    age: 35,
+    jobTitle: "programmer",
+    gender: "male",
+    youLiveInSkopje:"true",
+}
