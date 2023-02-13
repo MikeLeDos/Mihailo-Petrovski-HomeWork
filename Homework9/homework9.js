3705a9757df483d00aad91cdeea98be8eeb57b10
MikeLeDos <mikipetrovski@gmail.com>
2023-02-13 10:32:02 +0100

    Homework9

---
+++ Homework9/homework9.js
@@ -0,0 +1,73 @@
+//1,0,"1","0","ten",true,false,null,undefined,"",''
+let number1=1;
+console.log("Boolean :" + Boolean(number1), "String : "+ String(number1));
+
+let number2=0;
+console.log("Number :" + Number(number2), " String : " + String(number2));
+
+let number3="1";
+console.log("Number :" + Number(number3), "Boolean :" + Boolean(number3));
+
+let number4="0";
+console.log("Number :" + Number(number4), "Boolean :" + Boolean(number4));
+
+let number5="ten"
+console.log("Number :" + Number(number5), "Boolean :" + Boolean(number5));
+
+let number6=true;
+console.log("Number :" + Number(number6), "String :" + String(number6));
+
+let number7=false;
+console.log("Number :" + Number(number7), "String :" + String(number7));
+
+let number8=null;
+console.log("Number :" + Number(number8), "Boolean :" + Boolean(number8));
+
+let number9=undefined;
+console.log("Number :" + Number(number9), "String :" + String(number9));
+
+let number10="";
+console.log("Number :" + Number(number10), "Boolean :" + Boolean(number10));
+
+let number11='';
+console.log("Number :" + Number(number11), "Boolean :" + Boolean(number11));
+
+
+//2ra zadaca
+
+let bornDay = 12;
+console.log(bornDay);
+let sumNumberMyFullName = 16;
+console.log(sumNumberMyFullName);
+let birthdayYear=1984;
+let x=19;
+let y=84;
+let z=(y-x);
+console.log(z);
+let sumAllTheNumbers =1984;
+console.log(1+9+8+4);
+//var.5
+let birthdayDay=12;
+let sumMyFullName=16;
+console.log(Boolean(birthdayDay>sumMyFullName));
+
+//var.6
+let a=65;
+let b=22;
+console.log(a/b);
+
+//var.7
+let var7='I am the'
+if(Boolean === true && console.log(a/b)<3){
+    console.log(var7 + "I am the light");
+} else{
+    console.log(var7 + " "+ "darkness");
+}
+
+
+
+
+
+
+
+
